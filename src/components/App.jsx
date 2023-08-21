0818f36149bfa22c5f418ebd307986873e3d9bac
YuriyBeregovoy <rik0681@gmail.com>
2023-08-21 15:59:21 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,30 @@
 export const App = () => {
+  state = {
+    query: "",
+    images: [],
+    page: 1
+}
+
+  chageQuery = () => { };
+
+
+
+
+
   return (
     <div>
+      <div>
+        <form action="">
+          <input type="text" />
+          <button></button>
+        </form>
+      </div>
 
+      <div>Gallery</div>
+      <div>
+        <button>Load more</button>
+      </div>
 
-      
     </div>
   );
 };
