f3105fb1a2f2b237e172cf78676454a4e61a07af
YuriyBeregovoy <rik0681@gmail.com>
2023-08-23 15:41:06 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -6,6 +6,10 @@ axios.defaults.baseURL = "https://pixabay.com/api/"
 
 export const fetchImages = async (searchQuery) => {
   const KEY = '38213838-3daddfe3507b21e9b34121ca2';
-  const response =await axios.get(`/?q=${searchQuery}&page=${currentPage}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}`)
+  
+    // const response =await axios.get(`/?page=${currentPage}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}`)
+
+  
+  const response = await axios.get(`/?q=${searchQuery}&page=${currentPage}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}`)
   return response.data;
 }
