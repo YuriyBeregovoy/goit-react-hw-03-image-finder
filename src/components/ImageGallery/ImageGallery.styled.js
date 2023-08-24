65e7b33df99139ce04fe63f835fee03483da4f1f
YuriyBeregovoy <rik0681@gmail.com>
2023-08-24 22:48:35 +0300

    Create ImageGallery.styled.js

---
+++ src/components/ImageGallery/ImageGallery.styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+
+export const GalleryStyle = styled.ul`
+       
+
+        display: flex;
+        flex-wrap: wrap;
+        column-gap: 10px;
+        row-gap: 20px;
+        justify-content: center;
+
+`;
