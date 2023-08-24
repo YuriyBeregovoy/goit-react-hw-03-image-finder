74f9e05a63f1e8d88cc68a32825b81fef10ee069
YuriyBeregovoy <rik0681@gmail.com>
2023-08-24 22:48:38 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,9 +1,10 @@
 import { ImageGalleryItem } from "components/ImageGalleryItem/ImageGalleryItem"
+import { GalleryStyle } from "./ImageGallery.styled"
 
 export const ImageGallery = ({imagesArea, openModal}) => {
 
-  return (<ul>{imagesArea.map(image => (
+  return (<GalleryStyle>{imagesArea.map(image => (
     <ImageGalleryItem key={image.id} image={image} openModal={openModal} />
-  ))}</ul>)
+  ))}</GalleryStyle>)
 
 }
