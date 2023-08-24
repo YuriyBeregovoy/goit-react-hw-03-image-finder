b7472fc335951d1f34de68b121f213383bb52d36
YuriyBeregovoy <rik0681@gmail.com>
2023-08-24 23:26:02 +0300

    Create Searchbar.styled.js

---
+++ src/components/Searchbar/Searchbar.styled.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+
+export const SearchContainer = styled.div`
+       
+        position: sticky;
+        top: 0;
+        background-color: #ffffff;
+        z-index: 999;
+      
+  `;
+
+  export const SearchForm = styled.form`
+       display: flex;
+        justify-content: center;
+        margin-top: 20px;
+        margin-bottom: 20px;
+        gap: 20px;
+
+        font-family: 'Roboto', 'sans-serif';
+
+        color: #434455;
+     
+      
+  `;
