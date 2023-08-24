1f294d262efe40a70a7a5dfd74e859c129f35a5d
YuriyBeregovoy <rik0681@gmail.com>
2023-08-25 00:45:52 +0300

    Update LoadMoreButton.jsx

--- src/components/Button/LoadMoreButton.jsx
+++ src/components/Button/LoadMoreButton.jsx
@@ -1,3 +1,5 @@
+import { BtnLoadMore } from "./LoadMoreButton.styled";
+
 export const LoadMoreButton = ({onClick}) => {
-  return (<button type="button" onClick={onClick}>Load more</button>);
+  return (<BtnLoadMore type="button" onClick={onClick}>Load more</BtnLoadMore>);
 }
